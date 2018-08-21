39b6341fb26dfe8d758039052aaf2b2837ee44e5
purplenimbus <you@example.com>
2018-08-21 12:55:12 -0700

    return a list of all customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Customer as Customer;
 
 class CustomerController extends Controller
 {
-    //
+    public function getCustomers(Request $request){
+    	return Customer::paginate(10);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v'.env('API_VERSION',1))->group(function () {
+	Route::post('/login','Auth\LoginController@authenticate');
+	Route::get('/customers','CustomerController@getCustomers');
+});
+
+
