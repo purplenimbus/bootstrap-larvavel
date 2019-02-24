994c563655d5191649631282808b5e0bf2604d99
purplenimbus <you@example.com>
2019-02-24 13:44:37 -0800

    add application model

---
+++ app/Application.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Webpatser\Uuid\Uuid as Uuid;
+
+class Application extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'applicant_id',
+        'job_id',
+    ];
+
+    /**
+	 *  Setup model event hooks
+	 */
+	public static function boot()
+	{
+		parent::boot();
+		self::creating(function ($model) {
+			$model->uuid = (string) Uuid::generate(4);
+		});
+	}
+}
